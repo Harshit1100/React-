52e5b79de1f51b457d765d1ef5bc7033fcf9678b
Harshit1100 <95674901+Harshit1100@users.noreply.github.com>
2025-07-21 20:55:52 +0530

    Update home.jsx

---
+++ home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function TheMainHome() {
+  return (
+    <div>
+      <h1>🎉 Hello and welcome to my shop thing</h1>
+      <p>pls look around... maybe you like stuff 😅</p>
+
+      {}
+      <img src="https://via.placeholder.com/400x180" alt="banner for something" />
+
+      {}
+    </div>
+  );
+}
+
+export default TheMainHome;
+
